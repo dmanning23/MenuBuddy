09b537baa1d23fd82e16fa2527770a15c38a146c
dan-ubuntu <dan@ubuntu.(none)>
2013-04-22 19:19:22 -0700

    linux projects

--- Linux/Properties/AssemblyInfo.cs
+++ Linux/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("daniel.manning")]
+[assembly: AssemblyCopyright("dan")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
