72a781e2e95fa0415fb3f7c75669ccc61f1645e0
unknown <dmanning23@hotmail.com>
2015-04-11 09:38:04 -0500

    added options for doing stack alignement

---
+++ Source/Layouts/StackAlignment.cs
@@ -0,0 +1,13 @@
+namespace MenuBuddy
+{
+	/// <summary>
+	/// Different ways to align the items in a stack layout
+	/// </summary>
+	public enum StackAlignment
+	{
+		Top, //the stack will have vertical alignment and items will be added to the bottom
+		Bottom, //the stack will have vertical alignment and items will be added to the top
+		Left, //the stack will have horizontal alignment and items will be added to the right
+		Right //the stack will have horizontal alignment and items will be added to the left
+	}
+}
--- Source/Layouts/StackLayout.cs
+++ Source/Layouts/StackLayout.cs
@@ -8,22 +8,92 @@ namespace MenuBuddy
 	/// </summary>
 	public class StackLayout : Layout
 	{
+		#region Properties
+
+		/// <summary>
+		/// How to layout the items in this stack
+		/// </summary>
+		public StackAlignment Alignment { get; set; }
+
+		#endregion //Properties
+
 		#region Methods
 
-		public StackLayout()
+		public StackLayout(StackAlignment alignment = StackAlignment.Top)
 		{
+			Alignment = alignment;
 		}
 
 		public override void AddItem(IScreenItem item)
 		{
+			switch (Alignment)
+			{
+				case StackAlignment.Top:
+				{
+					AddBottom(item);
+				}
+				break;
+				case StackAlignment.Bottom:
+				{
+					AddTop(item);
+				}
+				break;
+				case StackAlignment.Left:
+				{
+					AddRight(item);
+				}
+				break;
+				case StackAlignment.Right:
+				{
+					AddLeft(item);
+				}
+				break;
+			}
+		}
+
+		private void AddTop(IScreenItem item)
+		{
 			//get the current rect
 			Rectangle rect = Rect;
 
-			//get the item rect
-			var itemRect = item.Rect;
+			//set the position of the new item
+			item.Position = new Point(Position.X, rect.Top - item.Rect.Height);
+
+			//store the new item
+			Items.Add(item);
+		}
+
+		private void AddBottom(IScreenItem item)
+		{
+			//get the current rect
+			Rectangle rect = Rect;
 
 			//set the position of the new item
 			item.Position = new Point(Position.X, rect.Bottom);
+
+			//store the new item
+			Items.Add(item);
+		}
+
+		private void AddLeft(IScreenItem item)
+		{
+			//get the current rect
+			Rectangle rect = Rect;
+
+			//set the position of the new item
+			item.Position = new Point(rect.Left - item.Rect.Width, Position.Y);
+
+			//store the new item
+			Items.Add(item);
+		}
+
+		private void AddRight(IScreenItem item)
+		{
+			//get the current rect
+			Rectangle rect = Rect;
+
+			//set the position of the new item
+			item.Position = new Point(rect.Right, Position.Y);
 
 			//store the new item
 			Items.Add(item);
