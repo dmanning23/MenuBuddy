976a5ea463e647d4e7764657f42c3810e92ca8bc
Dan Manning <dmanning23@hotmail.com>
2015-10-30 17:35:42 -0500

    cleaning up messagebox screen

---
+++ Source/Screens/IWidgetScreen.cs
@@ -0,0 +1,20 @@
+using GameTimer;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+
+namespace MenuBuddy
+{
+	/// <summary>
+	/// This the interface for a screen that can display a bunch of widgets.
+	/// </summary>
+	public interface IWidgetScreen : IScreenItemContainer
+	{
+		#region Methods
+
+		void OnSelect(object obj, PlayerIndexEventArgs playerIndex);
+
+		void OnCancel(object obj, PlayerIndexEventArgs playerIndex);
+
+		#endregion //Methods
+	}
+}
