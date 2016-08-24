52ff7797020c7760d12a0e41d37a7f17cdfac853
Dan Manning <dmanning23@hotmail.com>
2016-08-23 22:47:55 -0500

    added drag drop button

---
+++ MenuBuddy/MenuBuddy.Tests/DragDropTests.cs
@@ -0,0 +1,85 @@
+﻿using InputHelper;
+using Microsoft.Xna.Framework;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuBuddy.Tests
+{
+	[TestFixture]
+	public class DragDropTests
+	{
+		#region Fields
+
+		private DragDropButton _button;
+
+		#endregion //Fields
+
+		#region Setup
+
+		[SetUp]
+		public void Setup()
+		{
+			StyleSheet.InitUnitTests();
+			_button = new DragDropButton();
+		}
+
+		#endregion //Setup
+
+		#region tests
+
+		[Test]
+		public void Position_Default()
+		{
+			_button.Position = new Point(10, 20);
+			_button.Size = new Vector2(200f, 100f);
+			Assert.AreEqual(10, _button.Rect.X);
+			Assert.AreEqual(20, _button.Rect.Y);
+			Assert.AreEqual(200f, _button.Rect.Width);
+			Assert.AreEqual(100f, _button.Rect.Height);
+			Assert.AreEqual(HorizontalAlignment.Left, _button.Horizontal);
+			Assert.AreEqual(VerticalAlignment.Top, _button.Vertical);
+		}
+
+		[Test]
+		public void Position_ContainsEvent()
+		{
+			_button.Position = new Point(10, 20);
+			_button.Size = new Vector2(200f, 100f);
+
+			var e = new DragEventArgs
+			{
+				Start = new Vector2(20f, 40f)
+			};
+
+			Assert.IsTrue(_button.CheckDrag(e));
+		}
+
+		[Test]
+		public void Position_Moved()
+		{
+			_button.Position = new Point(10, 20);
+			_button.Size = new Vector2(200f, 100f);
+
+			var e = new DragEventArgs
+			{
+				Start = new Vector2(20f, 40f),
+				Current = new Vector2(50, 100)
+			};
+
+			_button.CheckDrag(e);
+
+			Assert.AreEqual(50, _button.Rect.X);
+			Assert.AreEqual(100, _button.Rect.Y);
+			Assert.AreEqual(200f, _button.Rect.Width);
+			Assert.AreEqual(100f, _button.Rect.Height);
+			Assert.AreEqual(HorizontalAlignment.Left, _button.Horizontal);
+			Assert.AreEqual(VerticalAlignment.Top, _button.Vertical);
+		}
+
+		#endregion //tests
+	}
+}
---
+++ Source/Widgets/Buttons/DragDropButton.cs
@@ -0,0 +1,63 @@
+using InputHelper;
+using System;
+
+namespace MenuBuddy
+{
+	/// <summary>
+	/// This is a button thhat contains a relaitve layout
+	/// </summary>
+	public class DragDropButton : RelativeLayoutButton, IDraggable
+	{
+		#region Properties
+
+		public event EventHandler<DragEventArgs> OnDrag;
+
+		#endregion //Properties
+
+		#region Initialization
+
+		/// <summary>
+		/// Constructs a new menu entry with the specified text.
+		/// </summary>
+		public DragDropButton()
+		{
+		}
+
+		public DragDropButton(DragDropButton inst) : base(inst)
+		{
+		}
+
+		/// <summary>
+		/// Get a deep copy of this item
+		/// </summary>
+		/// <returns></returns>
+		public override IScreenItem DeepCopy()
+		{
+			return new DragDropButton(this);
+		}
+
+		#endregion //Initialization
+
+		#region Methods
+
+		public bool CheckDrag(DragEventArgs drag)
+		{
+			var result = Rect.Contains(drag.Start);
+			if (result)
+			{
+				//Move the button to the current drag position
+				Position = drag.Current.ToPoint();
+
+				//fire off the event for any listeners
+				if (OnDrag != null)
+				{
+					OnDrag(this, drag);
+				}
+			}
+
+			return result;
+		}
+
+		#endregion
+	}
+}
